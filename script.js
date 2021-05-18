8a5294c887b76144791d57d1362304d1ab2d29a2
Avi <Inscription4@outlook.com>
2021-05-18 14:50:21 +0545

    intro finish

--- script.js
+++ script.js
@@ -1,144 +1,55 @@
-// The algorithm that runs the whole game, getHumanInput() and getComputerInput() are fed into it
-function gameLogic(playerSelection, computerSelection) {
+const btn = document.querySelector("button");
+startBtn.addEventListener('click', afterClick);
 
-let playerChoice = playerSelection;
-let computerChoice = computerSelection();
+function playAudio() {
+    const audio = document.querySelector('audio');
+    audio.play();
+}
 
-    if(playerChoice === 'rock') {
+function timeOuts1() {
 
-        if(computerChoice === 'rock') {
-            return("Tie");
-        } else if(computerChoice === 'paper') {
-            return("Computer wins");
-        } else if(computerChoice === 'scissors') {
-            return("Human wins");
-        }
+    const div = document.querySelector("#startBtnReplacer")
+    div.classList.add("startBtnReplacerStyle")
 
+    setTimeout(function(){
+        div.classList.remove("startBtnReplacerStyle")
+        div.classList.add("transitionStop-1")
+    }, 5600)
+    
+    setTimeout(function(){
+        div.classList.add("transition-2")
+    }, 7000)
 
-    } else if(playerChoice === 'paper') {
+    setTimeout(function() {
+        div.remove();
+        const body = document.querySelector("body");
+        body.style.backgroundColor= "black";
+        return true;
+    }, 10300)
 
-        if(computerChoice === 'paper') {
-            return("Tie");
-        } else if(computerChoice === 'rock') {
-            return("Human wins");
-        } else if(computerChoice === 'scissors') {
-            return("Computer wins");
-        } 
+}
 
+function afterClick() {
+    btn.remove();
 
-    } else if(playerChoice === 'scissors') {
+    playAudio();
+    timeOuts1();
+    
+    
+}
 
-        if(computerChoice === 'scissors') {
-            return("Tie");
-        } else if(computerChoice === 'rock') {
-            return("Computer wins");
-        } else if(computerChoice === 'paper') {
-            return("Human wins");
-        } 
+function makeBody() {
 
+    const headerDiv = document.querySelector("#header-div");
+    const buttonsDiv = document.querySelector("#buttonsDiv");
+    const buttons = document.querySelectorAll('button');
 
-    } else {
-        return("Don't be naughty! you know that weapon isn't allowed");
+    for(let i=0; i<buttons.length; i++) {
+
+        buttons[i].classList.remove('none');
     }
-    
+
+    console.log(buttons);
 
 }
-
-function onButtonClick(e) {
-
-    let input = getHumanInput(e);
-    let logicStorer = gameLogic(input, getComputerInput);
-    pointKeeper(logicStorer);
-
-}
-
-function getHumanInput(e) {
-
-    const clickedBtn = document.querySelector(`button[id="${e.explicitOriginalTarget.id}"]`);
-    
-    if(!clickedBtn) return;
-    else return clickedBtn.id;   
-
-}
-
-
-function getComputerInput() {
-    let choices = ["rock", "paper", "scissors"];
-
-    return choices[Math.floor((Math.random() * 3))];
-}
-
-
-function pointKeeper(matchWinner) {
-
-    if(matchWinner.indexOf("Human") != -1) {
-        ++humanPoints;
-        matchWinnerDisplayer("Human");
-    } else if(matchWinner.indexOf("Computer") != -1) {
-        ++computerPoints;
-        matchWinnerDisplayer("Computer");
-    } else if(matchWinner.indexOf("Tie") != -1) {
-        matchWinnerDisplayer("Tie");
-    }
-    
-    if(humanPoints == 5) {
-        pointsResetter();
-        verdictAnnouncer("Human");
-    } else if(computerPoints == 5) {
-        pointsResetter();
-        verdictAnnouncer("Computer")
-    }
-    
-}
-
-function matchWinnerDisplayer(str) {
-    let para;
-    
-    para = document.createElement('p');
-    para.textContent = `Point to ${str}`;
-    div.appendChild(para);
-}
-
-function pointsResetter() {
-    humanPoints = 0;
-    computerPoints = 0;
-}
-
-function verdictAnnouncer(winner) {
-    body.remove();
-
-    const html = document.querySelector("html");
-    const bodyNew = document.createElement("body");
-    const div = document.createElement("div");
-    const para = document.createElement("p");
-
-    para.textContent = `${winner} Wins`;
-
-    html.appendChild(bodyNew);
-    bodyNew.appendChild(div);
-    div.appendChild(para);
-};
-
-
-let div = document.createElement("div");
-let body = document.querySelector("body");
-
-body.appendChild(div);
-
-let humanPoints = 0;
-let computerPoints = 0;
-
-window.addEventListener('click', onButtonClick);
-
-
-
-
-
-
-
-
-
-    
-
-
 
